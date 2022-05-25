bbac4b88878607df0eef3b289962cd4547948bd1
Bautista Iorfida <bautistaiorfida@gmail.com>
2022-05-25 15:49:01 -0300

    Nuevo nav responsive para celu y carrusel de fotos

---
+++ app.js
@@ -0,0 +1,11 @@
+
+document.querySelector(".burgerButon").addEventListener("click", () => {
+
+    document.querySelector(".navCeluCompu").classList.toggle("show"); 
+
+})
+
+ScrollReveal().reveal('.contfotosMain');
+ScrollReveal().reveal('.mainCards', {delay:500});
+
+
